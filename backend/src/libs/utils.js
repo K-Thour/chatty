ac16cae49ffa0f1c26d2a2ec389c75e5bc1552d2
K-Thour <karanveerthour76@gmail.com>
2025-08-17 21:07:24 +0530

    fix(cors): cookies issue fixed

--- backend/src/libs/utils.js
+++ backend/src/libs/utils.js
@@ -9,7 +9,7 @@ export const generateJWT = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: "none",
   });
   return token;
 };
