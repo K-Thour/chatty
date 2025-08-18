bb1b75f827e024d992ea0cb7631a787866bdfb42
Karanveer <karanveer@devronins.com>
2025-08-18 10:12:45 +0530

    working on cookie issue

--- backend/src/middlewares/protect.middleware.js
+++ backend/src/middlewares/protect.middleware.js
@@ -4,6 +4,7 @@ import User from "../models/user.model.js";
 const protect = async (req, res, next) => {
   try {
     const token = req.cookies.token;
+    console.log(token);
     if (!token) {
       return res.status(401).json({ message: "Unauthorized access" });
     }
